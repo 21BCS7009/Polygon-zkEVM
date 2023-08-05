6a1fb30ae864fc3d85c31e85f9466a068e5d0d10
Mayank  Varshney <21BCS7009@cuchd.in>
2023-08-05 06:41:54 +0000

    Project Completed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,8 @@ import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-circom";
 // circuits
 import circuits = require('./circuits.config.json')
+import "dotenv/config";
+
 
 // set env var to the root of the project
 process.env.BASE_PATH = __dirname;
@@ -21,6 +23,12 @@ const config: HardhatUserConfig = {
         version: "0.6.11",
       }
     ]
+  },
+  networks: {
+    mumbai: {
+      url: "https://rpc.ankr.com/polygon_mumbai",
+      accounts: [process.env.PRIVATE_KEY]
+    }
   },
   circom: {
     // (optional) Base path for input files, defaults to `./circuits/`
